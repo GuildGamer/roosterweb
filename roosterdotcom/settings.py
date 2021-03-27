8869295db20009091f5b2e82852cf9c60e2c1cef
GuildGamer <tobi4steve@gmail.com>
2021-03-26 20:44:30 -0700

    removed argon2 from list of hashers

--- roosterdotcom/settings.py
+++ roosterdotcom/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    #'django.contrib.auth.hashers.Argon2PasswordHasher',
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     'django.contrib.auth.hashers.BCryptPasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
